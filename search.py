b3959ec937a31fa5a6863c3d773c8ef78797eead
xuwen-ho <ho.xu.wen@gmail.com>
2024-03-11 18:28:36 +0800

    Calculate cosine similarity based on vectorised query and vectorised json data

--- search.py
+++ search.py
@@ -1,4 +1,6 @@
 import json
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 # Load the JSON data
 with open('dicoding_courses.json', encoding='utf-8') as f:
@@ -11,4 +13,13 @@ corpus = []
 for course in courses:
     corpus.append(' '.join([course['name'], course['summary'], course['description']]).lower())
     
-print(corpus[1])
+# Vectorise data to enable us to compute cosine similarity    
+vectorizer = TfidfVectorizer()
+tfidf_matrix = vectorizer.fit_transform(corpus)
+
+# Prompt the user for the search query
+query = input("Enter your search query: ").lower()
+query_vector = vectorizer.transform([query])
+
+# Compute the cosine similarity
+similarity_scores = cosine_similarity(query_vector, tfidf_matrix)
